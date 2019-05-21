f02c1a959c48e4ca7c6b46cab26335c1e0e9babc
rukok <rukokuro@gmail.com>
2019-05-21 15:28:20 +0200

    Complete First Lesson

---
+++ app/src/main/java/com/example/artem/kotlinrealtimelocationedmtlesson/Utils/Common.kt
@@ -0,0 +1,11 @@
+package com.example.artem.kotlinrealtimelocationedmtlesson.Utils
+
+import com.example.artem.kotlinrealtimelocationedmtlesson.Model.User
+
+object Common {
+
+    val USER_UID_SAVE_KEY: String = "SAVE_KEY"
+    val TOKENS: String = "Tokens"
+    lateinit var loggedUser: User
+    val USER_INFO: String = "UserInfo"
+}
