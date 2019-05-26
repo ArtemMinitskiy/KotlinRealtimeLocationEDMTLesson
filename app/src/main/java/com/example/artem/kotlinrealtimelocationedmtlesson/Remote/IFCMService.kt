6d5950822bbfe039167815940fe083e51aff557a
Artem <rukokuro@gmail.com>
2019-05-26 18:38:06 +0200

    Update IFCMService.kt

--- app/src/main/java/com/example/artem/kotlinrealtimelocationedmtlesson/Remote/IFCMService.kt
+++ app/src/main/java/com/example/artem/kotlinrealtimelocationedmtlesson/Remote/IFCMService.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface IFCMService {
-    @Headers("Content-Type:application/json", "Authorization:key=AAAAoa9GbSI:APA91bE0R2zZN1drM04MXXOOEMbXsxNSgkne63o5QKSnWtV_5AREInFKr-e9ekLQsJqO20vPJrndlS0v2LCdT70Sf8xh-OlP_4Z_j-NK0wZoi5tIcM_YwVyazJ3B5eyVS9tZbrHkSyip")
+    @Headers("Content-Type:application/json", "Authorization:key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
     @POST("fcm/send")
     fun sendFriendRequestToUser(@Body body: Request):Observable<MyResponse>
 
